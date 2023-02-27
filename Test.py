2bb1fa2a1fd4795ccb16bd0e09de0808c219d1aa
Admin <zhonglinokami@gmail.com>
2023-02-27 14:38:02 +0700

    add

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 from parrot import Parrot
 import torch
 import warnings
+
 warnings.filterwarnings("ignore")
 
 ''' 
@@ -13,17 +14,40 @@ def random_state(seed):
 random_state(1234)
 '''
 
-#Init models (make sure you init ONLY once if you integrate this to your code)
-parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5")
 
-phrases = ["Can you recommend some upscale restaurants in Newyork?",
-           "What are the famous places we should not miss in Russia?"
-]
+class GPTRewriteParrot():
+    # Init models (make sure you init ONLY once if you integrate this to your code)
+    parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5")
 
-for phrase in phrases:
-  print("-"*100)
-  print("Input_phrase: ", phrase)
-  print("-"*100)
-  para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False)
-  for para_phrase in para_phrases:
-    print(para_phrase)
+    def __init__(self):
+        pass
+
+    def paraphrase(self, phrases):
+        result = ''
+        for paragraph in phrases.split('\n'):
+            if paragraph.strip() == '':
+                result += '\n'
+                continue
+            for phrase in paragraph.split('.'):
+                if phrase.strip() == '':
+                    continue
+                adequacy_threshold = 0.90
+                has_phase = False
+                while not has_phase and adequacy_threshold > 0:
+                    try:
+                        para_phrases = self.parrot.augment(input_phrase=phrase,
+                                                           use_gpu=False,
+                                                           do_diverse=True,  # Enable this to get more diverse paraphrases
+                                                           adequacy_threshold=adequacy_threshold,
+                                                           # Lower this numbers if no paraphrases returned
+                                                           fluency_threshold=0.80)
+                        if len(para_phrases) > 0:
+                            if (para_phrases[0][0].strip() != ''):
+                                has_phase = True
+                                result += para_phrases[0][0].strip().capitalize() + '. '
+                    except:
+                        pass
+                    finally:
+                        adequacy_threshold -= 0.05
+            result += '\n'
+        return result
